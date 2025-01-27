d150d38055cdf487117b1cf60b8cc4d53397a9c9
Jesus24-Dev <siritjesus24@gmail.com>
2025-01-27 13:37:37 -0400

    User model entity created

--- src/main/java/com/jesus24dev/myblog/persistence/models/User.java
+++ src/main/java/com/jesus24dev/myblog/persistence/models/User.java
@@ -1,17 +1,98 @@
 
 package com.jesus24dev.myblog.persistence.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import java.util.Date;
 
 @Entity
 public class User {
     
+    @Id
     private String email;
+    
+    @Column(unique = true)
     private String nickname;
+    
     private String name;
     private String lastname;
     private Date birthday;
     private String gender;
+    
+    @OneToOne
+    private Profile profile;
+
+    public User() {
+    }
+
+    public User(String email, String nickname, String name, String lastname, Date birthday, String gender, Profile profile) {
+        this.email = email;
+        this.nickname = nickname;
+        this.name = name;
+        this.lastname = lastname;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.profile = profile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+    
     
 }
