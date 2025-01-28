5fd5af340864cc9936f97e07cde6fbb451d60aa1
Jesus24-Dev <siritjesus24@gmail.com>
2025-01-28 13:51:10 -0400

    Persistence Class created

---
+++ src/main/java/com/jesus24dev/myblog/persistence/services/PersistanceServices.java
@@ -0,0 +1,9 @@
+
+package com.jesus24dev.myblog.persistence.services;
+
+public class PersistanceServices {
+    UserServices userServices = new UserServices();
+    ProfileServices profileServices = new ProfileServices();
+    PostServices postServices = new PostServices();
+    CommentServices commentServices = new CommentServices();
+}
