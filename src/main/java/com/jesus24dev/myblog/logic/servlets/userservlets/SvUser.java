95cbf7ab0a99bd88cd361e72e4639328d4c4953f
Jesus24-Dev <siritjesus24@gmail.com>
2025-01-30 10:23:23 -0400

    Servlet for log in created

---
+++ src/main/java/com/jesus24dev/myblog/logic/servlets/userservlets/SvUser.java
@@ -0,0 +1,59 @@
+
+package com.jesus24dev.myblog.logic.servlets.userservlets;
+
+import com.jesus24dev.myblog.logic.controllers.ProfileController;
+import com.jesus24dev.myblog.logic.controllers.UserController;
+import com.jesus24dev.myblog.persistence.models.User;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(name = "SvUser", urlPatterns = {"/SvUser"})
+public class SvUser extends HttpServlet {
+    UserController userController = new UserController();
+    ProfileController profileController = new ProfileController();
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");            
+            User user = userController.getUser(email);
+            
+            if(user == null){
+                request.setAttribute("error", "user not founded");
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+            } else {
+                if (user.getPassword().equals(password)){
+                    HttpSession userSession = request.getSession();
+                    userSession.setAttribute("profileId", user.getProfile().getId());
+                    response.sendRedirect("home.jsp");
+                } else {
+                    request.setAttribute("error", "password don't match");
+                    request.getRequestDispatcher("index.jsp").forward(request, response);
+                }                
+            }        
+    }
+        
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
