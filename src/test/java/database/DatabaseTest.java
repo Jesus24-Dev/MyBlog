fc730255cff1141f7e95ac52717c18bbd2c90da1
Jesus24-Dev <siritjesus24@gmail.com>
2025-01-30 08:58:18 -0400

    Change variables data type from Date to LocalDate

--- src/test/java/database/DatabaseTest.java
+++ src/test/java/database/DatabaseTest.java
@@ -6,9 +6,8 @@ import com.jesus24dev.myblog.persistence.models.Post;
 import com.jesus24dev.myblog.persistence.models.Profile;
 import com.jesus24dev.myblog.persistence.models.User;
 import com.jesus24dev.myblog.persistence.services.PersistenceServices;
-import com.jesus24dev.myblog.persistence.services.UserServices;
 import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public class DatabaseTest {
@@ -28,7 +27,7 @@ public class DatabaseTest {
         //Create empty post's list
         List<Post> postList = new ArrayList<>();
         
-        Date datePost = new Date();
+        LocalDate datePost = LocalDate.now();
         post.setDescription("This is a new post");
         post.setPublishedAt(datePost);
         post.setProfile(profile);
@@ -40,7 +39,7 @@ public class DatabaseTest {
         List<Comment> commentList = new ArrayList<>();
                                       
         //Create new comment
-        Date dateComment = new Date();
+        LocalDate dateComment = LocalDate.now();
         Comment comment = new Comment("This is a new comment", dateComment, profile, post);
         ps.commentServices.createComment(comment);
         
@@ -57,8 +56,8 @@ public class DatabaseTest {
         ps.profileServices.editProfile(profile);
         
         //Create new User instance
-        Date date = new Date();
-        User user = new User("subjecttest@gmail.com", "JohnDoe123", "John", "Doe", date, "Male", profile);        
+        LocalDate date = LocalDate.of(2004, 4, 24);
+        User user = new User("subjecttest@gmail.com", "JohnDoe123", "John", "Doe","123456", date, "Male", profile);        
         
         //Add user in database       
         ps.userServices.createUser(user);
