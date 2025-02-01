6b10c4b053b0d94c12125c2243b98690d4248b55
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-01 09:08:39 -0400

    servlet to profile done

---
+++ src/main/java/com/jesus24dev/myblog/logic/servlets/profileservlets/SvProfile.java
@@ -0,0 +1,46 @@
+
+package com.jesus24dev.myblog.logic.servlets.profileservlets;
+
+import com.jesus24dev.myblog.persistence.models.Profile;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(name = "SvProfile", urlPatterns = {"/SvProfile"})
+public class SvProfile extends HttpServlet {
+       
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            HttpSession session = request.getSession();
+            Long id = Long.parseLong(request.getParameter("id"));
+            Profile profile = (Profile) session.getAttribute("profile");
+            
+            if(id == profile.getId()){
+                System.out.println("COINCIDEN");
+            }
+            response.sendRedirect("profile.jsp?id="+id);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
