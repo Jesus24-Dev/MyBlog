b320d98b149f2c3ea4d18060229be049020b038c
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-01 11:09:10 -0400

    Seeker logic in servlet post done

--- src/main/java/com/jesus24dev/myblog/logic/servlets/postservlets/SvPost.java
+++ src/main/java/com/jesus24dev/myblog/logic/servlets/postservlets/SvPost.java
@@ -2,6 +2,7 @@
 package com.jesus24dev.myblog.logic.servlets.postservlets;
 
 import com.jesus24dev.myblog.logic.controllers.PostController;
+import com.jesus24dev.myblog.logic.controllers.SeekerController;
 import com.jesus24dev.myblog.persistence.models.Post;
 import com.jesus24dev.myblog.persistence.models.Profile;
 import java.io.IOException;
@@ -12,10 +13,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 @WebServlet(name = "SvPost", urlPatterns = {"/SvPost"})
 public class SvPost extends HttpServlet{
     PostController postController = new PostController();
+    SeekerController seekerController = new SeekerController();
     
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -27,7 +30,23 @@ public class SvPost extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    
+        
+        String text = request.getParameter("text");
+        
+        if (text.trim() != ""){
+            ArrayList<Long> postId = seekerController.getPostId(text);
+            ArrayList<Post> postList = new ArrayList();
+        
+            for(Long id: postId){
+                Post post = postController.getPost(id);
+                postList.add(post);
+            }
+            request.setAttribute("postList", postList);
+        } else {
+            request.setAttribute("postList", null);
+        }
+                             
+        request.getRequestDispatcher("searchPost.jsp").forward(request, response);
     }
     
     @Override
