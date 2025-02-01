851f31f4fce4224d2a8d268125f36e9e0e201812
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-01 10:48:45 -0400

    AAdd seeker controller in attributes

--- src/main/java/com/jesus24dev/myblog/persistence/services/PersistenceServices.java
+++ src/main/java/com/jesus24dev/myblog/persistence/services/PersistenceServices.java
@@ -1,11 +1,14 @@
 
 package com.jesus24dev.myblog.persistence.services;
 
+import com.jesus24dev.myblog.persistence.controllers.SeekerController;
+
 public class PersistenceServices {
     public UserServices userServices = new UserServices();
     public ProfileServices profileServices = new ProfileServices();
     public PostServices postServices = new PostServices();
     public CommentServices commentServices = new CommentServices();
+    public SeekerController seekerController = new SeekerController();
 
     public PersistenceServices() {
     }
